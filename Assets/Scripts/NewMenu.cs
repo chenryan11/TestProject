ca53972f4d78d37e419fc3cd0cda698311aaa3cd
Ryan <chenryan11@yahoo.com.tw>
2020-05-31 03:30:31 +0800

    新增測試UI

---
+++ Assets/Scripts/NewMenu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewMenu : MonoBehaviour
+{
+    public GameObject PlayerHUDCanvus = null;
+
+    //public Button btnNewGame;
+
+    /*void Start()
+    {
+        if (PlayerHUDCanvus != null)
+        {
+            int childCount = PlayerHUDCanvus.transform.childCount;
+
+            for (int i = 0; i < childCount; i++)
+            {
+                GameObject child = PlayerHUDCanvus.transform.GetChild(i).gameObject;
+
+                if (child.name.Equals("NewGame"))
+                {
+                    (child.GetComponent<Button>())
+                        .onClick
+                        .AddListener(btnNewGame);
+                }
+            }
+        }
+    }*/
+    /*
+    void btnNewGame()
+    {
+        UnityEngine.Debug.Log("123");
+    }*/
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            UnityEngine.Debug.Log("123");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //UnityEngine.Debug.Log("123");
+
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           // UnityEngine.Debug.Log("123");
+        }
+    }
+} 
